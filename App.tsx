b7b4e75721b86267f78d6cceea055fcdd0249919
Igor Monteiro <igornathandev@gmail.com>
2021-07-13 21:19:17 -0300

    First

--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/router';
+import AppLoading from 'expo-app-loading'
+import { useFonts, Anton_400Regular } from '@expo-google-fonts/anton'
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Anton_400Regular
+  });
+
+  if(!fontsLoaded) return <AppLoading />
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" backgroundColor="#000" translucent />
+      <Routes />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
